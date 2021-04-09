9d37edd2a0856a2c41165e4bc6ce8233a8c867b7
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2021-04-09 19:02:56 +0800

    Delete nkn2.sh

--- nkn2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-screen_name=$"nkn"
-screen -dmS $screen_name
-cmd=$"./nknd -p yuqing@U1";
-screen -x -S $screen_name -p 0 -X stuff "$cmd"
-screen -x -S $screen_name -p 0 -X stuff $'\n'
-echo ok
