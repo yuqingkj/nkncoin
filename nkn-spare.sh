7a114ff5c3fdfe7d2da2ebf212e8f39a6c0add14
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2021-04-17 17:40:40 +0800

    Add files via upload

---
+++ nkn-spare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo -i <<EOF
+wget http://18.189.189.198:8000/nkn-v2.0.8.tar.gz
+tar -xvf nkn-v2.0.8.tar.gz
+cd nkn-v2.0.8/
+wget http://18.189.189.198:8000/nkn-v2.0.8/ChainDB_Latest.tar.gz
+tar -xvf ChainDB_Latest.tar.gz
+chmod 777 ./nkn*
+rm -rf ./ChainDB_Latest.tar.gz
+./nknc wallet -c -p yuqing@U1
+apt update
+apt install supervisor -y
+supervisord -c ./supervisord.conf
+EOF
