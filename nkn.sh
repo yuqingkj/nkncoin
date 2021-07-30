3eb30b51ad8a9975b2bd1727fd7bc7a668f322a9
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2021-07-30 11:13:31 +0800

    Update nkn.sh

--- nkn.sh
+++ nkn.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo -i <<EOF
-wget http://3.95.133.91:8000/nkn_latest.tar.gz
+wget http://45.79.206.208:8000/nkn_latest.tar.gz
 tar -xvf nkn_latest.tar.gz
 cd nkn/
-wget http://3.95.133.91:8000/ChainDB_Latest.tar.gz
+wget http://45.79.206.208:8000/ChainDB_Latest.tar.gz
 tar -xvf ChainDB_Latest.tar.gz
 chmod 777 ./nkn*
 rm -rf ./ChainDB_Latest.tar.gz
